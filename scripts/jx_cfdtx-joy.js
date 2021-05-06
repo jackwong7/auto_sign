20d16c29599b1f40c8417706a604715176d5b291
Joy <Joy>
2021-05-06 09:20:27 +0800

    123

--- scripts/jx_cfdtx-joy.js
+++ scripts/jx_cfdtx-joy.js
@@ -54,7 +54,7 @@ $.userName = '';
     if ($.currentCookie) {
       $.userName =  decodeURIComponent($.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1]);
       $.log(`\n开始【京东账号${i + 1}】${$.userName}`);
-      
+
       await cashOut();
     }
   }
@@ -68,8 +68,8 @@ function cashOut() {
     $.get(
       taskUrl(
         `consume/CashOut`,
-        `ddwMoney=100&dwIsCreateToken=0&ddwMinPaperMoney=100000&strPgtimestamp=${$.currentToken['timestamp']}&strPhoneID=${$.currentToken['phoneid']}&strPgUUNum=${$.currentToken['farm_jstoken']}`
-      ), 
+        `ddwMoney=100&dwIsCreateToken=0&ddwMinPaperMoney=150000&strPgtimestamp=${$.currentToken['timestamp']}&strPhoneID=${$.currentToken['phoneid']}&strPgUUNum=${$.currentToken['farm_jstoken']}`
+      ),
       async (err, resp, data) => {
         try {
           $.log(data);
@@ -85,7 +85,7 @@ function cashOut() {
       }
     );
   });
-} 
+}
 
 function taskUrl(function_path, body) {
   return {
