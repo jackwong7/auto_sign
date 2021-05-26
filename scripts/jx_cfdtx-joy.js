23c18045b93bf09aed0d896b7011b19a9c866ad3
JackWong <wangchangat@163.com>
2021-05-26 16:33:11 +0800

    Update jx_cfdtx-joy.js

--- scripts/jx_cfdtx-joy.js
+++ scripts/jx_cfdtx-joy.js
@@ -24,7 +24,7 @@ $.userName = '';
   for (let i = 0; i < $.cookieArr.length; i++) {
     $.currentCookie = $.cookieArr[i];
     $.currentToken = $.tokenArr[i];
-    if ($.currentCookie) {
+    if ($.currentCookie && $.currentToken) {
       $.userName =  decodeURIComponent($.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1]);
       $.log(`\n开始【京东账号${i + 1}】${$.userName}`);
 
